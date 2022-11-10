cd8e3aad18f4c084de88b5968ff8f96b8794b66c
DilanFernando <dilan.vista@gmail.com>
2022-11-10 23:51:37 +0530

    test removed

--- src/test/java/lk/icoder/restdockerapi/RestDockerApiApplicationTests.java
+++ src/test/java/lk/icoder/restdockerapi/RestDockerApiApplicationTests.java
@@ -1,12 +1,11 @@
 package lk.icoder.restdockerapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestDockerApiApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
